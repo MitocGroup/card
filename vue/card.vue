079163d068b075523533349e7cc714c47d93a26a
AlexanderC <alexander.moldova@gmail.com>
2017-01-05 16:59:28 +0200

    Add vue.js v2 component

---
+++ vue/card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="jp-card-box">
+    <slot name="preview">
+      <div class="jp-card-preview"></div>
+    </slot>
+  </div>
+</template>
+
+<script>
+import Card from '../src/coffee/card.coffee'
+
+export default {
+  props: {
+    options: Object
+  },
+  
+  data () {
+    return {
+      $card: null
+    }
+  },
+  
+  mounted () {
+    this.$nextTick(() => {
+      this.options.container = this.options.container || '.jp-card-preview'
+      
+      this.$card = new Card(this.options)
+    })
+  }
+}
+</script>
